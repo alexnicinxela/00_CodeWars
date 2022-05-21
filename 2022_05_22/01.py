87abec714c8fcbe2f4c46cd0fba71e428887cbc1
Alexandr N <nogovitsin.ae@yandex.ru>
2022-05-22 02:01:39 +0300

    2022_05_22_02 + upd

--- 2022_05_22/01.py
+++ 2022_05_22/01.py
@@ -1,3 +1,7 @@
+'''Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+It should remove all values from list a, which are present in list b keeping their order.'''
+
 def array_diff(a, b):
     for num in a[::-1]:
         if num in b: del a[a.index(num)]
